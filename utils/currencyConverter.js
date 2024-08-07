204c553fbd83f8930b9e31f69cad81de9f62b701
Sahil Rana <xsranhail@gmail.com>
2024-08-07 17:29:40 +0530

    change1

--- utils/currencyConverter.js
+++ utils/currencyConverter.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export const convertCurrency = async (amount, fromCurrency, toCurrency) => {
-    // Add logic to convert currency using an external API
-    // Example:
-    // const response = await axios.get(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`);
-    // const rate = response.data.rates[toCurrency];
-    // return amount * rate;
+
     return amount; // Placeholder
 };
